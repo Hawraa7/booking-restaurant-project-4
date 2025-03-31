ec8cca51786f6d188dc9f13e2ab0070dc0710ff7
hawraa7 <hawraahijazi1996@gmail.com>
2025-03-31 07:37:10 +0200

    added cloudinary configuration properly

--- bookingrestaurant/settings.py
+++ bookingrestaurant/settings.py
@@ -165,3 +165,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cloudinary storage settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
